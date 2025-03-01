2300ee797f62e1b837a3d1545861ffb4418daace
Dhyey <dhyeypf@gmail.com>
2025-03-01 17:47:10 +0530

    come at header

---
+++ app/(auth)/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthoLayout = ({children}) => {
+  return (
+    <div className='flex items-center justify-center pt-16'>{children}</div>
+  )
+}
+
+export default AuthoLayout
